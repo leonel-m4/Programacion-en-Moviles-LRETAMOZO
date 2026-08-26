2a15f13cac42c6c4c4db46efab1b31c72bbbe189
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-26 10:01:45 -0500

    Se declara las variables del cliente y la lista Carrito

--- LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
@@ -4,6 +4,11 @@ fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+
+    val nombreCliente = "Juan Leon" // String (inferido)
+    val carrito = mutableListOf<Producto>() // lista vacía de productos
+    println("Cliente: $nombreCliente")
+    println()
 }
 
 data class Producto(
