0f1700615292117024e863adc8fdb470ae8400fb
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-26 09:57:14 -0500

    Lab02 Carrito Kotlin

--- LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/MainActivity.kt
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.retamozo.lab02carritokotlin.ui.theme.Lab02CarritoKotlinTheme
+import com.retamozo.lab02carritokotlin.ui.theme.Lab02carritokotlinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab02CarritoKotlinTheme {
+            Lab02carritokotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Lab02CarritoKotlinTheme {
+    Lab02carritokotlinTheme {
         Greeting("Android")
     }
 }
--- LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/ui/theme/Theme.kt
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Lab02CarritoKotlinTheme(
+fun Lab02carritokotlinTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
