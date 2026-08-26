8f83c9059bc4bd420d6489c43fa36c80d6cf048f
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-26 10:41:50 -0500

    Agrega reporte de detalle con columnas alineadas

--- LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
@@ -19,6 +19,7 @@ fun main() {
         println("Producto agregado: ${producto.nombre}")
     }
 
+    mostrarDetalle(carrito)
     val subtotal = calcularSubtotal(carrito)
     val igv = calcularIGV(subtotal)
     val total = calcularTotal(subtotal,igv)
@@ -48,4 +49,15 @@ fun calcularIGV(subtotal: Double): Double {
 
 fun calcularTotal(subtotal: Double, igv: Double): Double {
     return subtotal + igv
+}
+
+fun mostrarDetalle(productos: List<Producto>) {
+    println("--------- DETALLE DEL CARRITO ---------")
+    var i = 1
+    for (p in productos) {
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d S/ %8.2f", i, p.nombre, p.cantidad, importe))
+        i++
+    }
+    println("---------------------------------------")
 }
