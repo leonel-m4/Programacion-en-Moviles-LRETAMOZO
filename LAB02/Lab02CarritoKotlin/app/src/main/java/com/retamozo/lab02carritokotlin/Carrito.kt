94c6e07b40de6329c77869d1e7db21c6a3e7f533
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-26 10:22:09 -0500

    Se agregaron funciones de cálculo

--- LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
@@ -25,3 +25,19 @@ data class Producto(
     val precio: Double,
     var cantidad: Int
 )
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    return subtotal * 0.18
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
+}
