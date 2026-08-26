01b9bfa74f114c0e55b5b8c6e87a3c5576a2d525
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-26 08:38:39 -0500

    Proyecto inicial con el menú del carrito

---
+++ LAB02/Lab02CarritoKotlin/app/src/main/java/com/retamozo/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.retamozo.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
