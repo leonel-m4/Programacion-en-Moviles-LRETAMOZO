789e5952d3448980bfffa47c020c23bb33597bd2
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-30 22:30:11 -0500

    Agrega datos del prestamo (nombre, tipo usuario, fechas)

--- LAB03/Sistem de Multas - Leonel Retamozo/app/build.gradle.kts
+++ LAB03/Sistem de Multas - Leonel Retamozo/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.retamozo.sistemdemultas_leonelretamozo"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
--- LAB03/Sistem de Multas - Leonel Retamozo/app/src/main/java/com/retamozo/sistemdemultas_leonelretamozo/multas.kt
+++ LAB03/Sistem de Multas - Leonel Retamozo/app/src/main/java/com/retamozo/sistemdemultas_leonelretamozo/multas.kt
@@ -1,15 +1,53 @@
 package com.retamozo.sistemdemultas_leonelretamozo
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun main(){
     println("=========================================")
     println("Sistema de Multas")
     println("=========================================")
+
+    print("Titulo del libro: ")
+    val nombreLibro = readLine()!!
+    println()
+
+    print("Nombre del usuario: ")
+    val nombreUsuario = readLine()!!
+    println()
+
+    println("Tipo de usuario:")
+    println("1. Alumno")
+    println("2. Docente")
+    print("Selecciona el tipo de usuario: ")
+    val opcionTipo=readLine()!!.toInt()
+    val tipoUsuario = when(opcionTipo){
+        1 -> "Alumno"
+        2 -> "Docente"
+        else -> "No válido"
+    }
+    println()
+
+    val formato = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    print("Fecha de prestamo(dd/MM/yyyy): ")
+    val FechaPrestamo = LocalDate.parse(readLine()!!,formato)
+    print("Fecha de entrega(dd/MM/yyyy): ")
+    val FechaEntrega = LocalDate.parse(readLine()!!,formato)
+    print("Fecha de devolucion(dd/MM/yyyy): ")
+    val FechaDevolucion = LocalDate.parse(readLine()!!,formato)
+    println()
+
+    println("Titulo del libro: $nombreLibro")
+    println("Nombre del usuario: $nombreUsuario")
+    println("Tipo de usuario: $tipoUsuario")
+    println("Fecha de prestamo: $FechaPrestamo")
+    println("Fecha de entrega: $FechaEntrega")
+    println("Fecha de devolucion: $FechaDevolucion")
 }
 
 data class PrestamoLibro(
     val nombreLibro: String,
     val nombreUsuario: String,
+    val tipoUsuario:String,
     val FechaPrestamo: LocalDate,
     val FechaDevolucion: LocalDate,
     val FechaEntrega: LocalDate,
