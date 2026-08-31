22d9c2801a63e5736b4e803d90b553122ebbe23b
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-08-30 21:10:20 -0500

    Creación de la estructura de datos

--- LAB03/Sistem de Multas - Leonel Retamozo/app/src/main/java/com/retamozo/sistemdemultas_leonelretamozo/multas.kt
+++ LAB03/Sistem de Multas - Leonel Retamozo/app/src/main/java/com/retamozo/sistemdemultas_leonelretamozo/multas.kt
@@ -1,7 +1,16 @@
 package com.retamozo.sistemdemultas_leonelretamozo
+import java.time.LocalDate
 
 fun main(){
     println("=========================================")
     println("Sistema de Multas")
     println("=========================================")
 }
+
+data class PrestamoLibro(
+    val nombreLibro: String,
+    val nombreUsuario: String,
+    val FechaPrestamo: LocalDate,
+    val FechaDevolucion: LocalDate,
+    val FechaEntrega: LocalDate,
+)
