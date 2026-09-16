667605c2ac5d7e62c377807df419e722f4797979
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-16 00:36:00 -0500

    Crear data class Tarea y eliminar código de ejemplo en MainActivity

--- LAB04/Lista de Tareas/app/src/main/java/com/retamozo/listadetareas/MainActivity.kt
+++ LAB04/Lista de Tareas/app/src/main/java/com/retamozo/listadetareas/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,35 +14,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.retamozo.listadetareas.ui.theme.ListaDeTareasTheme
 
+data class Tarea(
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListaDeTareasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme() {
+                PantallaTareas()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ListaDeTareasTheme {
-        Greeting("Android")
     }
 }
