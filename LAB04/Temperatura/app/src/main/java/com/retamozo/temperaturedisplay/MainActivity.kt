a95c0c44b9fd0d0b7c5aceed179b26ab8795131c
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-16 02:52:17 -0500

    Agregar estado inicial de temperatura

--- LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
+++ LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.retamozo.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,10 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TemperatureDisplayTheme {
-                Scaffold {
-                    TemperatureDisplay()
-                }
+            MaterialTheme() {
+                TemperatureDisplay()
             }
         }
     }
@@ -24,5 +27,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TemperatureDisplay() {
-
+    var temperatura by remember { mutableStateOf(20) }
 }
