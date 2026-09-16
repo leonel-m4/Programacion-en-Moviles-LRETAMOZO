31e0fc7bc93602e9df5e5a0f683794e06b642c14
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-16 01:32:45 -0500

    Crear composable PantallaTareas con estado, agregar tarea y lista

--- LAB04/Lista de Tareas/app/src/main/java/com/retamozo/listadetareas/MainActivity.kt
+++ LAB04/Lista de Tareas/app/src/main/java/com/retamozo/listadetareas/MainActivity.kt
@@ -5,19 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,6 +85,70 @@ fun ItemTarea(
             }
             Button(onClick = onEliminar) {
                 Text("Eliminar")
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaTareas(){
+    var textoTarea by remember { mutableStateOf("") }
+    var contadorld by remember { mutableStateOf(1) }
+    val listaTareas = remember { mutableStateListOf<Tarea>() }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Lista de tareas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = textoTarea,
+            onValueChange = {textoTarea=it},
+            label = {Text("Ingrese una tarea")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                if (textoTarea.isNotBlank()){
+                    listaTareas.add(
+                        Tarea(
+                            id=contadorld,
+                            nombre = textoTarea
+                        )
+                    )
+                    contadorld++
+                    textoTarea=""
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Agrega Tarea")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Total de tareas:${listaTareas.size}",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn{
+            items(listaTareas, key = {it.id}){tarea ->
+                ItemTarea(
+                    tarea=tarea,
+                    onEliminar = {
+                        listaTareas.remove(tarea)
+                    },
+                    onCambiarEstado = {completada ->
+                        val index = listaTareas.indexOf(tarea)
+                        if (index!= -1){
+                            listaTareas[index]=listaTareas[index].copy(completada = completada)
+                        }
+                    }
+                )
             }
         }
     }
