ee01cd20f9eea30692bc3e38584ca588467344c0
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-16 02:48:49 -0500

    Crear composable TemperatureDisplay

--- LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
+++ LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.retamozo.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TemperatureDisplayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold {
+                    TemperatureDisplay()
                 }
             }
         }
@@ -31,17 +23,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TemperatureDisplay() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TemperatureDisplayTheme {
-        Greeting("Android")
-    }
 }
