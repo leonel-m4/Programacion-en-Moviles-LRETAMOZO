1808116130e85a69cc4748052e98d3834de20c9e
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-16 02:55:13 -0500

    Mostrar temperatura en pantalla

--- LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
+++ LAB04/Temperatura/app/src/main/java/com/retamozo/temperaturedisplay/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.retamozo.temperaturedisplay.ui.theme.TemperatureDisplayTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,4 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TemperatureDisplay() {
     var temperatura by remember { mutableStateOf(20) }
+
+    Text(
+        text = "Temperatura: $temperatura °C"
+    )
 }
