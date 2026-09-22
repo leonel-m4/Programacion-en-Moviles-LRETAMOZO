4c7a50c63461b3b531dacc6f72083d9922f934d5
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-22 01:41:42 -0500

    Implementar AppNavigation.kt y MainActivity.kt

--- LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/MainActivity.kt
+++ LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.retamozo.portalacademico.navigation.AppNavigation
 import com.retamozo.portalacademico.ui.theme.PortalAcademicoTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PortalAcademicoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PortalAcademicoTheme {
-        Greeting("Android")
     }
 }
--- LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/navigation/AppNavigation.kt
+++ LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/navigation/AppNavigation.kt
@@ -1,2 +1,41 @@
 package com.retamozo.portalacademico.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun AppNavigation(){
+    val navController= rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
