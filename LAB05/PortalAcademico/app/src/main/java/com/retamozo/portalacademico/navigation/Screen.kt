e1ec3481d1855e0d8815c972a1cc637206f5a31b
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-22 01:19:50 -0500

    Agregar Screen.kt con sealed class para definir rutas de navegación

--- LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/navigation/Screen.kt
+++ LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/navigation/Screen.kt
@@ -1,4 +1,11 @@
 package com.retamozo.portalacademico.navigation
 
-class Screen {
+sealed class Screen(val route: String){
+    object Home: Screen("home")
+    object List: Screen("list")
+    object Profile: Screen("profile")
+
+    object Detail: Screen("detail/{itemId}"){
+        fun createRoute(itemId: Int): String="detail/$itemId"
+    }
 }
