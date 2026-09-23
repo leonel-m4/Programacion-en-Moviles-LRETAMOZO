3af9a23b0cba6981de10dce97560350eebf3b653
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-22 19:26:35 -0500

    App finalizada del Portal Academico

---
+++ LAB05/PortalAcademico/app/src/main/java/com/retamozo/portalacademico/screens/ProfileScreen.kt
@@ -0,0 +1,51 @@
+package com.retamozo.portalacademico.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.retamozo.portalacademico.navigation.Screen
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Mi perfil",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Leonel Mathias Retamozo De la Cruz",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = {
+                navController.navigate(Screen.Home.route){
+                    popUpTo(Screen.Home.route) {inclusive = true}
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ir al inicio")
+        }
+    }
+}
