d3dc11a7bb7c3300132f0311a3c86503d373c99f
Leonel <leonelmathiasretamozoxd@gmail.com>
2026-09-23 05:08:29 -0500

    Configurar MainActivity

--- LAB05/TecsupFit/app/src/main/java/com/retamozo/tecsupfit/MainActivity.kt
+++ LAB05/TecsupFit/app/src/main/java/com/retamozo/tecsupfit/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.retamozo.tecsupfit.navigation.AppNavigation
 import com.retamozo.tecsupfit.ui.theme.TecsupFitTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TecsupFitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TecsupFitTheme {
-        Greeting("Android")
     }
 }
